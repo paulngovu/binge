2e5d6a82b0653fb1658e266e6cccd85a01a43f98
Ryan Lin <ryanlin95129@gmail.com>
2021-11-27 18:31:45 -0800

    connect chat with database

--- pages/chats.jsx
+++ pages/chats.jsx
@@ -1,39 +1,218 @@
-import ChatFeed from '../components/ChatFeed';
+// import ChatFeed from '../components/ChatFeed';
+
+import Layout from '../components/Layout';
+import chatMessages from '../components/chatMessages';
+
+import {
+  Box,
+  Button,
+  Grid,
+  Text,
+  TextInput
+} from 'grommet';
+import { Send } from 'grommet-icons';
+
+import { useRef, useEffect, useState } from 'react';
+import useKeypress from 'react-use-keypress';
 
 import { PrismaClient, Message, Prisma } from '@prisma/client';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 const prisma = new PrismaClient();
 
 export async function getServerSideProps() {
-  const messages = await prisma.message.findMany({
+  let messages = await prisma.message.findMany({
     where: {
       username: {
         equals: "user"
       }
     }
   });
-  messages.map(mes => {
+  messages = messages.map(mes => {
     mes.timeSent = JSON.parse(JSON.stringify(mes.timeSent));
     return mes;
   })
+
+  let groupedMessages = messages.reduce((r, a) => {
+    r[a.foodname] = r[a.foodname] || [];
+    r[a.foodname].push(a)
+    return r;
+  }, Object.create(null));
+
+  const likes = await prisma.like.findMany({
+    where: {
+      username: {
+        equals: "user"
+      }
+    }
+  })
+  let likedFoods = likes.map(like => like.foodname)
+
   return {
     props: {
-      allMessages: messages
+      allMessages: groupedMessages,
+      foodChats: likedFoods
     }
   };
 }
 
-async function saveMessage(message) {
+async function saveMessage(message, foodname, sentByUser) {
+  var isoDate = new Date().toISOString();
+  const messageInstance = {
+    sentByUser: sentByUser,
+    username: "user",
+    foodname: foodname,
+    message: message,
+    timeSent: isoDate
+  };
+
+
   const response = await fetch('/api/messages', {
     method: 'POST',
-    body: JSON.stringify(message)
+    body: JSON.stringify(messageInstance)
   })
 }
 
-export default function Chats({ allMessages }) {
+export default function Chats({ allMessages, foodChats }) {
+  // scroll to bottom of overflow
+  // https://stackoverflow.com/questions/37620694/how-to-scroll-to-bottom-in-react
+  const messagesEndRef = useRef(null);
+  const [currentChat, setCurrentChat] = useState(foodChats[0]);
+  const [chatMessage, setChatMessage] = useState("");
   const [messages, setMessages] = useState(allMessages);
-  return <ChatFeed existingMessages={ messages }/>
-}
+
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+  }
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [currentChat]);
+  
+  const onSubmit = async () => {
+    // post message to database and clear input line
+    try {
+      await saveMessage(chatMessage, currentChat, true);
+      console.log(chatMessage);
+      setChatMessage("");
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useKeypress(['Enter'], () => {
+    onSubmit();
+    scrollToBottom();
+  });
+
+  return (
+    <Layout buttons={["home"]}>
+      <Grid
+          rows={['auto', 'xsmall']}
+          columns={['small', 'auto']}
+          areas={[
+            { name: 'sidebar', start: [0, 0], end: [0, 1] },
+            { name: 'messages', start: [1, 0], end: [1, 0] },
+            { name: 'input', start: [1,1], end: [1,1] },
+          ]}
+          height="88vh"
+      >
+      <Box 
+        background="dark-4" 
+        gridArea="sidebar" 
+        border={true}
+      >
+        {
+          // messages is a map of foodid => array of all message objects
+          // between current user and food of foodid
+          foodChats.map(foodName => (
+          <Box 
+            key={foodName}
+            border={"bottom"} 
+            background={currentChat == foodName ? "light-4" : ""}
+            onClick={() => setCurrentChat(foodName)}
+            focusIndicator={false}
+          >
+            <Text 
+              margin="medium" 
+              color={currentChat == foodName ? "black" : "white"}
+            >
+              {foodName}
+            </Text>
+          </Box> )
+          )
+        }
+      </Box>
+      <Box 
+        background="light-2" 
+        gridArea="messages" 
+        overflow="auto" 
+        border={true}
+      >
+        {
+          Object.keys(messages).map((key) =>
+            key == currentChat ?
+              messages[key].map(msg => 
+                <div 
+                  key={msg.id}
+                  style={{
+                    border: "1px solid black",
+                    borderRadius: "2vw",
+                    margin: msg.sentByUser? "0vh 1vw 2vh 5vw" : 
+                                            "0vh 5vw 2vh 1vw",
+                    background: msg.sentByUser ? "#3471eb" : "light-4",
+                    padding: "1vw"
+                }}
+                >
+                  <Text color={msg.sentByUser ? "white" : "black"}>
+                    {msg.timeSent}
+                  </Text>
+                  <br />
+                  <Text color={msg.sentByUser ? "white" : "black"}>
+                    {msg.message}
+                  </Text>
+                </div>
+              )
+            : null)
+        }    
+        {/* dummy div to pin bottom of messages */}
+        <div ref={messagesEndRef}></div>
+      </Box>
+      <Box gridArea="input" border={true}>
+        <Grid
+          rows={['auto']}
+          columns={['auto', 'xxsmall']}
+          areas={[
+            { name: 'message', start: [0, 0], end: [0,0] },
+            { name: 'send', start: [1, 0], end: [1, 0] }
+          ]}
+          height="100%"
+        >
+          <Box gridArea="message">
+          <TextInput
+            plain={true}
+            focusIndicator={false}
+            value={chatMessage}
+            placeholder={"send your message here..."}
+            onChange={(e) => setChatMessage(e.target.value)}
+          />
+          </Box>
+          <Box gridArea="send" align="center" justify="center">
+            <Button 
+              icon={<Send color='black' />}
+              onClick={() => onSubmit()}
+              hoverIndicator
+              tip={{
+                content: "press enter/click here to send"
+              }}
+            />
+          </Box>
+        </Grid>
+      </Box>
+    </Grid>
+  </Layout>
+  );
+};
 
 
