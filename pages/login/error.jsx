5ffa8819c10ac93e8398fed8f7c9d4310e7b2635
Miles Kang <milesjkang@gmail.com>
2021-11-28 20:01:53 -0800

    Add cute hamburger to error page

--- pages/login/error.jsx
+++ pages/login/error.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Text } from 'grommet';
+import { Box, Button, Image, Text } from 'grommet';
 import Layout from '../../components/Layout';
 import { PATH_LOGIN } from '../../paths';
 import { TESTID_LOGIN_ERROR_BUTTON } from '../../testIds';
@@ -7,6 +7,12 @@ const LoginError = () => (
   <Layout>
     <Box pad='large'>
       <div className='container'>
+        <Box height='xsmall'>
+          <Image
+            src='https://media.istockphoto.com/vectors/cartoon-cute-burger-icon-isolated-on-white-background-vector-id1142855781?k=20&m=1142855781&s=170667a&w=0&h=rBuRzFRXoS9jcc1FRL-rSexSP97wNsxhzJ-feGTeuJ4='
+            fit="contain"
+          />
+        </Box> 
         <Text size='large'>You must log in.</Text>
         <Box pad='small'>
           <Button
